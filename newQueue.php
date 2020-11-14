e95dfe453a2fbc223c615df59e4852a3f6264a93
Mngz <mngz636@gmail.com>
2020-11-14 18:36:03 +0200

    Update newQueue.php

--- newQueue.php
+++ newQueue.php
@@ -2,8 +2,16 @@
 session_start();
 $conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
 
-$sql = 'INSERT INTO queue (id,member_id,product_id) VALUES (0,'.$_SESSION['customer_id'].','.$_GET['product_id'].');';
+$sql = 'SELECT id FROM queue WHERE member_id='.$_SESSION['customer_id'].' AND product_id='.$_GET['product_id'];
 $result = $conn->query($sql);
 
-echo $result?'Success':'Failure';
+  if($row = $result->fetch_assoc()){
+    echo 'Already in queue.';
+  }else{
+    $sql = 'INSERT INTO queue (id,member_id,product_id) VALUES (0,'.$_SESSION['customer_id'].','.$_GET['product_id'].');';
+    $result = $conn->query($sql);
+
+    echo ($result?'Success':'Failure');
+  }
+
 ?>
