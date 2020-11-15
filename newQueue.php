50e3a69fe7e9f0567ea8b9f21e48cf0a60223492
Mngz <mngz636@gmail.com>
2020-11-15 19:42:17 +0200

    Update newQueue.php

--- newQueue.php
+++ newQueue.php
@@ -1,7 +1,11 @@
 <?php
+
 session_start();
+
 $conn = new mysqli('localhost','produc10_mng','mngzpass636','produc10_productlists');
 
+if(ISSET($_SESSION['customer_id'])){
+  
 $sql = 'SELECT id FROM queue WHERE member_id='.$_SESSION['customer_id'].' AND product_id='.$_GET['product_id'];
 $result = $conn->query($sql);
 
@@ -13,5 +17,8 @@ $result = $conn->query($sql);
 
     echo ($result?'Success':'Failure');
   }
+}else{
+ echo 'Sign In'; 
+}
 
 ?>
